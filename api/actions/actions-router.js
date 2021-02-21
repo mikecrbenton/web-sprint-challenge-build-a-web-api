38862ecea4f5bea4d770060f3e19ef54dae199ab
mikecrbenton <mikecrbenton@hotmail.com>
2021-02-20 20:05:34 -0600

    actions router functional

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,8 +4,8 @@ const actionsModel = require("./actions-model")
 
 
 
-router.get("/actions", (req, res) => {
-   const actions = actionsModel.get()
+router.get("/actions", async (req, res) => {
+   const actions = await actionsModel.get()
    res.json(actions)
 })
 
@@ -23,4 +23,37 @@ router.get("/actions/:id", async (req, res) => {
 
 })
 
-module.exports = router             
+router.post("/actions", async (req,res) => {
+   const newAction = await actionsModel.insert( {
+      project_id: req.body.project_id,
+      description: req.body.description,
+      notes: req.body.notes
+   })
+   res.status(201).json(newAction)
+})
+
+router.put("/actions/:id", async (req,res) => {
+   const updatedAction = await actionsModel.update( req.params.id, {
+      project_id: req.body.project_id,
+      description: req.body.description,
+      notes: req.body.notes
+   })
+   res.status(201).json(updatedAction)
+})
+
+router.delete("/actions/:id", async (req,res) => {
+
+   const user = await actionsModel.get(req.params.id)
+
+   if(user) {
+      console.log(user)
+      await actionsModel.remove(user.id)
+      res.status(204).end() // successful empty response
+   }else{
+      res.status(404).json( {message: "User not found"} ) 
+   }
+
+})
+
+module.exports = router     
+
