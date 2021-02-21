c8027691065db99fb37661e1782de8979db6cdbe
mikecrbenton <mikecrbenton@hotmail.com>
2021-02-20 19:41:38 -0600

    figured out async/await

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -9,4 +9,18 @@ router.get("/actions", (req, res) => {
    res.json(actions)
 })
 
+router.get("/actions/:id", async (req, res) => {
+
+   // different way of accessing the params in express
+   const id = req.params.id
+   const action = await actionsModel.get(id) 
+   console.log(action)
+
+   if(action){
+      res.json(action) }
+   else {
+      res.status(404).json( {message: "User not found"} ) }
+
+})
+
 module.exports = router             
